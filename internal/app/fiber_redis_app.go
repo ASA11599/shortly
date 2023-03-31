9c8e8d0b863824503512b4a34ef58e47d8e3679a
Amayas Said Amer <asaid046@uottawa.ca>
2023-03-31 15:25:01 -0400

    Add Get and Set methods to FiberRedisApp

--- internal/app/fiber_redis_app.go
+++ internal/app/fiber_redis_app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/ASA11599/shortly/internal/handlers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/redis/go-redis/v9"
 )
@@ -47,13 +48,29 @@ func (fra *FiberRedisApp) initRedisClient() error {
 
 func (fra *FiberRedisApp) initFiberApp() error {
 	fra.fiberApp = fiber.New()
+	handlers.RegisterHandlers(fra.fiberApp)
 	return fra.fiberApp.Listen("0.0.0.0:8080")
 }
 
 func (fra *FiberRedisApp) closeRedisClient() error {
+	if fra.redisClient == nil { return nil }
 	return fra.redisClient.Close()
 }
 
 func (fra *FiberRedisApp) stopFiberApp() error {
+	if fra.fiberApp == nil { return nil }
 	return fra.fiberApp.Shutdown()
+}
+
+func (fra *FiberRedisApp) Set(key string, value string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	return fra.redisClient.Set(ctx, key, value, 0).Err()
+}
+
+func (fra *FiberRedisApp) Get(key string) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	cmd := fra.redisClient.Get(ctx, key)
+	return cmd.String(), cmd.Err()
 }
