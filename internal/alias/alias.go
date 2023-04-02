87664d14f05f9ccc2b1f87426300cd6dc6fdeb97
Amayas Said Amer <asaid046@uottawa.ca>
2023-04-02 12:38:53 -0400

    Implement function in alias.go

--- internal/alias/alias.go
+++ internal/alias/alias.go
@@ -1,6 +1,28 @@
 package alias
 
+import (
+	"math/rand"
+)
+
+func randomLetter() byte {
+	return byte(97 + rand.Intn(25))
+}
+
 func GenerateAlias() string {
 	// Generates a random string of 5 characters
-	return "g6Fcs"
+	return string([]byte{
+		randomLetter(),
+		randomLetter(),
+		randomLetter(),
+		randomLetter(),
+		randomLetter(),
+	})
+}
+
+func ValidateAlias(alias string) bool {
+	if len(alias) != 5 { return false }
+	for _, b := range alias {
+		if (b < 97) || (b > 122) { return false }
+	}
+	return true
 }
